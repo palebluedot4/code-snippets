3e30b0480b8565b8678fcfc77488a9745d86e012
Bruce <162610581+palebluedot4@users.noreply.github.com>
2025-08-13 08:47:00 +0800

    feat(go): add GCD function for big.Int GCD calculation
    Introduce a new GCD function that takes two big.Int pointers as
    arguments and returns their greatest common divisor. This function
    also handles nil arguments by returning an appropriate error. This
    enhancement improves the usability of the mathutil package for
    calculating GCD with large integers.

--- algorithms/math/go/gcd.go
+++ algorithms/math/go/gcd.go
@@ -1,5 +1,17 @@
 package mathutil
 
+import (
+	"errors"
+	"math/big"
+)
+
+func GCD(a, b *big.Int) (*big.Int, error) {
+	if a == nil || b == nil {
+		return nil, errors.New("gcd: nil argument")
+	}
+	return new(big.Int).GCD(nil, nil, a, b), nil
+}
+
 func GCDInt64(a, b int64) int64 {
 	a = Abs(a)
 	b = Abs(b)
