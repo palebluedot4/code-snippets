26843cd233af344cc4f1a1507999bb856f129100
Bruce <162610581+palebluedot4@users.noreply.github.com>
2025-08-27 09:26:36 +0800

    feat(go): add IsEvenBitwise function for bitwise checks
    Introduce IsEvenBitwise function to determine if a number is even
    using bitwise operations. This provides an alternative method for
    checking evenness, which can be more efficient in certain scenarios.

--- algorithms/math/go/even.go
+++ algorithms/math/go/even.go
@@ -5,3 +5,7 @@ import "golang.org/x/exp/constraints"
 func IsEven[T constraints.Integer](x T) bool {
 	return x%2 == 0
 }
+
+func IsEvenBitwise[T constraints.Integer](x T) bool {
+	return x&1 == 0
+}
