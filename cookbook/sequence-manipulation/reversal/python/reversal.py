79c83f23d4d38bad2854ccc3676a3b66841d5bb2
Bruce <162610581+palebluedot4@users.noreply.github.com>
2025-08-29 22:34:53 +0800

    feat(python): add manual reverse list in place function
    Introduce a new function to manually reverse a list in place
    using a two-pointer technique. This provides an alternative
    method for reversing lists without using the built-in reverse
    method, enhancing flexibility for list manipulation.

--- cookbook/sequence-manipulation/reversal/python/reversal.py
+++ cookbook/sequence-manipulation/reversal/python/reversal.py
@@ -17,3 +17,11 @@ def reversed_sequence(seq: Sequence[T]) -> List[T]:
 
 def reverse_list_in_place(lst: List[T]) -> None:
     lst.reverse()
+
+
+def _manual_reverse_list_in_place(lst: List[T]) -> None:
+    left, right = 0, len(lst) - 1
+    while left < right:
+        lst[left], lst[right] = lst[right], lst[left]
+        left += 1
+        right -= 1
