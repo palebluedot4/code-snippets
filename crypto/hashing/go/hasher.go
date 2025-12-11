d343969c980cb7caaad65be189e93b71465c27a6
Bruce <162610581+palebluedot4@users.noreply.github.com>
2025-12-11 09:25:14 +0800

    chore(go): add mock generation directive for Hasher
    Added a directive for generating mocks of the Hasher interface using
    the mockgen tool. This change facilitates easier testing by allowing
    for the creation of mock implementations of the Hasher interface,
    which can be used in unit tests to isolate dependencies and improve
    test coverage.

--- crypto/hashing/go/hasher.go
+++ crypto/hashing/go/hasher.go
@@ -4,6 +4,7 @@ import "errors"
 
 var ErrHashMismatch = errors.New("hash does not match the data")
 
+//go:generate go run go.uber.org/mock/mockgen -destination=mock_hasher_test.go -package=hashing -source=hasher.go Hasher
 type Hasher interface {
 	Hash(data []byte) ([]byte, error)
 	Verify(hash []byte, data []byte) error
