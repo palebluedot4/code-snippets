9eff217094fe9a7bdc0a16b7cd1b17a83877da6c
Bruce <162610581+palebluedot4@users.noreply.github.com>
2025-12-11 09:24:49 +0800

    chore(go): reorder ErrHashMismatch declaration
    Moved the ErrHashMismatch variable declaration to the top of the file
    for better visibility and organization. This change improves code
    readability and maintains a consistent structure within the
    hashing package.

--- crypto/hashing/go/hasher.go
+++ crypto/hashing/go/hasher.go
@@ -2,9 +2,9 @@ package hashing
 
 import "errors"
 
+var ErrHashMismatch = errors.New("hash does not match the data")
+
 type Hasher interface {
 	Hash(data []byte) ([]byte, error)
 	Verify(hash []byte, data []byte) error
 }
-
-var ErrHashMismatch = errors.New("hash does not match the data")
