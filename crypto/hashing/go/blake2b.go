8522a231bcaf393d787b777bcdea865d94eb1489
Bruce <162610581+palebluedot4@users.noreply.github.com>
2026-01-02 09:58:50 +0800

    feat(go): implement BLAKE2bHasher with Hash and Verify methods
    Add the BLAKE2bHasher structure that provides hashing and verification
    capabilities using the BLAKE2b algorithm. The Hash method computes the
    hash of the input data, while the Verify method checks if the provided
    hash matches the computed hash, returning an error if they do not match.
    This implementation enhances the cryptographic functionality of the
    hashing package.

--- crypto/hashing/go/blake2b.go
+++ crypto/hashing/go/blake2b.go
@@ -1,9 +1,28 @@
 package hashing
 
+import (
+	"crypto/subtle"
+
+	"golang.org/x/crypto/blake2b"
+)
+
 type BLAKE2bHasher struct{}
 
 var _ Hasher = (*BLAKE2bHasher)(nil)
 
 func NewBLAKE2bHasher() *BLAKE2bHasher {
 	return &BLAKE2bHasher{}
+}
+
+func (h *BLAKE2bHasher) Hash(data []byte) ([]byte, error) {
+	sum := blake2b.Sum256(data)
+	return sum[:], nil
+}
+
+func (h *BLAKE2bHasher) Verify(hash []byte, data []byte) error {
+	sum := blake2b.Sum256(data)
+	if subtle.ConstantTimeCompare(hash, sum[:]) != 1 {
+		return ErrHashMismatch
+	}
+	return nil
 }
