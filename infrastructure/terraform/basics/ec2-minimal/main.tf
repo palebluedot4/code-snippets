b9fd2d0a07870d7bbd6da46f4dfa361a3c5c917b
Bruce <162610581+palebluedot4@users.noreply.github.com>
2026-01-11 12:47:41 +0800

    feat(terraform): add EC2 minimal configuration
    Introduce a new Terraform configuration for deploying a minimal AWS EC2
    instance. This includes specifying the required AWS provider and version,
    defining the data source for the Amazon Linux AMI, and creating an EC2
    instance with the specified AMI and instance type. The output will return
    the instance ID upon creation. This setup serves as a foundational example
    for further infrastructure development.

---
+++ infrastructure/terraform/basics/ec2-minimal/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-kernel-6.1-arm64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t4g.micro"
+  tags = {
+    Name = "minimal-instance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
